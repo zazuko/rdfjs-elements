5494ff41302526a8f0ab1305cdab7c04bbfaf850
tpluscode <tpluscode@users.noreply.github.com>
2024-04-16 22:15:41 +0200

    build: usee vite

--- packages/rdfjs-elements-demos/webpack.config.cjs
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-const config = require('../../webpack.config.cjs')
-
-module.exports = config({
-  input: path.resolve(__dirname, './index.html'),
-  output: path.resolve(__dirname, '../../dist/converter'),
-})
